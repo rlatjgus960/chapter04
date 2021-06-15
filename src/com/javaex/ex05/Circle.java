0282fac8fef8ae5030273098febdb28f8ee3187c
rlatjgus960 <rlatjgus960@naver.com>
2021-06-15 17:08:57 +0900

    6/15일 ArrayList, LinkedList 구조 설명(호환)-인터페이스

---
+++ src/com/javaex/ex05/Circle.java
@@ -0,0 +1,33 @@
+package com.javaex.ex05;
+
+public class Circle {
+
+	// 필드
+	private int radius;
+
+	// 생성자
+	public Circle() {
+		super();
+	}
+
+	public Circle(int radius) {
+		super();
+		this.radius = radius;
+	}
+
+	// 메소드gs
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	// 메소드일반
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + "]";
+	}
+
+}
